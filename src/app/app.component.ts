af2faa0515875ab8239ea20df5e79029605e452b
tomi_farkas <tfarkas@szintezis.hu>
2021-06-28 07:58:32 +0200

    bitcoin tracker websocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
+import { of, Subscription } from 'rxjs';
+import { concatMap, delay } from 'rxjs/operators';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bitcoin-tracker';
+  rate: any;
+  Highcharts: typeof Highcharts = Highcharts;
+  chardata: any[] = [];
+  chartOptions: any;
+  subject = webSocket('wss://ws.coincap.io/prices?assets=bitcoin');
+  ngOnInit() {
+    this.rate = this.subject
+      .pipe(concatMap((item) => of(item).pipe(delay(1000))))
+      .subscribe((data) => {
+        this.rate = data;
+        this.chardata.push(Number(this.rate.bitcoin));
+        this.chartOptions = {
+          series: [
+            {
+              data: this.chardata,
+            },
+          ],
+          chart: {
+            type: 'line',
+            zoomType: 'x',
+          },
+          title: {
+            text: 'linechart',
+          },
+        };
+      });
+  }
 }
